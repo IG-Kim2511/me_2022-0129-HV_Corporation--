14de7c44711a8fe37898be85e6c3c838f3925b71
IG <iikim2511@gmail.com>
2022-02-05 11:02:46 -0500

    fin

--- script.js
+++ script.js
@@ -42,54 +42,38 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  // let dots = document.getElementsByClassName("dot");
 
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  } 
+
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+
+
+  // for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace(" active", "");
+  // }
+
+  slides[slideIndex-1].style.display = "block"; 
+
+
+  // 🍀
+  setInterval(() => {
+    // slideIndex++;    
+    // if (slideIndex >4) {
+    //   slideIndex = 1;    
+    // }  
+    plusSlides(1);
+  }, 2000);
+
+
+  // dots[slideIndex-1].className += " active";
 }
 
-
-// 🍀js30. slide-show - auto playing
-// (function(){
-//   var imgLen = document.getElementById('imgGallary');
-//   var images = imgLen.getElementsByTagName('img');
-
-//   let images = document.querySelectorAll('.slideshow img');
-//   let counter = 1;
-
-//   if(counter <= images.length){
-//       setInterval(function(){
-//           images[0].src = images[counter].src;
-//           console.log(images[counter].src);
-//           counter++;
-
-//           if(counter === images.length){
-//               counter = 1;
-//           }
-//       },4000);
-//   }
-// })();
-
-/* 
-img1.setAttribute('src', '/imgs/2-arrows.png');
-*/
-
-// 🍀youtube
-
-// let counter = 1
-// setInterval(() => {
-//   document.getElementById('radio'+counter).checked = true;
-//   counter++;
-//   if (counter >4) {
-//     counter = 1;    
-//   }
-  
-// }, 1000);
