632322b9888a4946bae4361a324296bd278b0bf5
IG <iikim2511@gmail.com>
2022-02-05 11:07:42 -0500

    fin

--- script.js
+++ script.js
@@ -3,33 +3,13 @@
 
 
 // 🍀js22. slide-show
-/* 
-const slides = document.querySelectorAll('.slide-container');
-let index = 0;
 
-console.log(slides);
-console.log(slides.length); //3
-
-function next() {
-    slides[index].classList.remove('active');
-     //🍉js22-40
-    index = (index+1) % slides.length; // 1 2 0 
-    console.log(index)
-    slides[index].classList.add('active');
-}
-
-function prev() {
-    slides[index].classList.remove('active');
-     //🍉js22-50
-    index = (index -1 + slides.length) % slides.length; //2 1 0 
-    console.log(index)
-    slides[index].classList.add('active');    
-}
- */
-
+/* onclick
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a> 
+*/
 
 let slideIndex = 1;
-showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -39,41 +19,39 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+showSlides(slideIndex);
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   // let dots = document.getElementsByClassName("dot");
-
+  
   if (n > slides.length) {
     slideIndex = 1
   } 
-
+  
   if (n < 1) {
     slideIndex = slides.length
   }
-
+  
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    slides[i].style.display = "none";  
   }
-
-
+  
+  
   // for (i = 0; i < dots.length; i++) {
-  //     dots[i].className = dots[i].className.replace(" active", "");
-  // }
-
-  slides[slideIndex-1].style.display = "block"; 
-
-
-  // 🍀
-  setInterval(() => {
-    // slideIndex++;    
-    // if (slideIndex >4) {
-    //   slideIndex = 1;    
-    // }  
-    plusSlides(1);
-  }, 2000);
-
-
-  // dots[slideIndex-1].className += " active";
+    //     dots[i].className = dots[i].className.replace(" active", "");
+    // }
+    
+    slides[slideIndex-1].style.display = "block"; 
+    
+    
+    // 🍉auto play
+    setInterval(() => { 
+      plusSlides(1);
+    }, 2000);
+    
+    
+    // dots[slideIndex-1].className += " active";
 }
 
+  
