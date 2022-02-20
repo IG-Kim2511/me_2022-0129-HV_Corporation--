cbee03a2837d2920deb4fcb22f6c8497bcc210bd
IG <iikim2511@gmail.com>
2022-02-20 13:36:00 -0500

    logo-change

--- nav2.js
+++ nav2.js
@@ -5,7 +5,7 @@ let nav2Elem = document.querySelector('.nav2');
 
 nav2Elem.innerHTML = `
   <div class="title">
-  <img src="../img/LOGO2.png" alt="Logo">
+    <img src="../img/logo-capture-horizontal.jpg" alt="Logo">
   </div>
 
   <div class="navbar">
@@ -23,26 +23,3 @@ nav2Elem.innerHTML = `
   </div>                      
  `;
 
-
-/* 
-        <div class="title">
-            <img src="../img/LOGO2.png" alt="Logo">
-        </div>
-
-        <div class="navbar">
-            <ul>
-                <a href="../index.html">메인</a>
-                <a href="./introduce.html">소개</a>
-                <a href="./team.html">팀원소개</a>
-                <a href="./service.html">서비스</a>
-                <a href="./contact.html">문의</a>
-                <a href="./faq.html">FAQ</a>                 
-            </ul>
-            <ul class="phone-container">
-                <li class="phone">jc@hvco.kr</li>
-                <li class="phone">+82 10 8079 2550</li>
-            </ul>
-        </div>
-
-
-*/
